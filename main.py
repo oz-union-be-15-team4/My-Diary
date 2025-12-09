ed71b600e70212cb6353228bd497de96aae8d90f
algorid <chlwlgns3855@naver.com>
2025-12-09 14:48:45 +0900

    템플릿 동기화

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from app.db.database import init_db
 from app.model.user import User
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="app/front")
 
 async def ensure_db():
     await init_db()
 
 
-@app.get("/")
-def root():
-    return {"message": "FastAPI 정상 작동 중 ✅"}
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    context = {
+        "request": request,
+    }
+    return templates.TemplateResponse("index.html", context)
 
 @app.get("/users")
 async def list_users(_=Depends(ensure_db)):
