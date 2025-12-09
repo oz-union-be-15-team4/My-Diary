30a3eb4c97d69327332ab43bc2eee7039cf0192b
Ryu-GY <a56322505@gmail.com>
2025-12-09 16:56:26 +0900

    re commit

---
+++ app/db/database.py
@@ -0,0 +1,25 @@
+# app/db/database.py
+import os
+from tortoise import Tortoise
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL = (
+    f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+)
+
+async def init_db():
+    print("DB INIT 시작")
+    await Tortoise.init(
+        db_url=DB_URL,
+        modules={
+            "models": ["app.model.user"]  # ✅ 폴더 통째로 지정
+        },
+    )
+    await Tortoise.generate_schemas()
+    print("DB INIT 완료")
+
+async def close_db():
+    await Tortoise.close_connections()
---
+++ app/model/user.py
@@ -0,0 +1,6 @@
+from tortoise import fields, models
+
+class User(models.Model):
+    id = fields.IntField(pk=True)
+    username = fields.CharField(max_length=50)
+    email = fields.CharField(max_length=100, unique=True)
